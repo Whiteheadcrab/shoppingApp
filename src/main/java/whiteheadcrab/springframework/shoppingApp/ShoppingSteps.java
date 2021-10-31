2f28f0af8f6ddf5c15c15686b9a2985a520af769
Whiteheadcrab <lettersorstudy@gmail.com>
2021-10-31 16:10:36 +0100

    Version 1.5 Added Scenario 2 . Added steps for Scenario 2 and also performed small refactoring

--- src/main/java/whiteheadcrab/springframework/shoppingApp/ShoppingSteps.java
+++ src/main/java/whiteheadcrab/springframework/shoppingApp/ShoppingSteps.java
@@ -7,7 +7,7 @@ import io.cucumber.java.en.When;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShoppingScenario1 {
+public class ShoppingSteps {
 
     @Given("^User start the application$")
     public void userStartApp() {
@@ -15,15 +15,33 @@ public class ShoppingScenario1 {
     }
 
     @Then("^In basket should be see selection of products$")
-    public void userviewBasketSelectionOfProducts()
+    public void userViewBasketSelectionOfProducts()
     {
         System.out.println("In basket should be see selection of products");
     }
 
-    @When("^User browse basket without buying product$")
+    @When("User browse basket without buying product")
     public void userViewBasketNoProducts()
     {
         System.out.println("User browse basket without buying product");
+    }
+
+    @Then("User's basket should not be empty")
+    public void userViewBasketNotEmpty()
+    {
+        System.out.println("User's basket should not be empty");
+    }
+
+    @And("User can buy an item")
+    public void userCanBuyItem()
+    {
+        System.out.println("User can buy an item");
+    }
+
+    @When("^User browse basket with buying product$")
+    public void userViewBasketWithProducts()
+    {
+        System.out.println("User browse basket with buying product");
     }
 
     @Then("^User's basket should be empty$")
--- src/main/resources/features/shopping.feature
+++ src/main/resources/features/shopping.feature
@@ -7,9 +7,9 @@ Feature:  Check my basket application
     Then User's basket should be empty
     And User cannot buy an item
 
-  Scenario: No item Selected in basket
-    Given User start the application
-    Then In basket should be see selection of products
-    When User browse basket with buying product
-    Then User's basket should be not empty
-    And User can buy an item
+    Scenario: Item selected in basket
+      Given User start the application
+      Then In basket should be see selection of products
+      When User browse basket with buying product
+      Then User's basket should not be empty
+      And User can buy an item

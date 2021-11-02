4774dcccd2695356b9b9aebdf8ced94d62aaef7b
Whiteheadcrab <lettersorstudy@gmail.com>
2021-11-02 18:12:57 +0100

    Version 1.7 Used parametrisation in steps and scenarios

--- src/main/java/whiteheadcrab/springframework/shoppingApp/ShoppingSteps.java
+++ src/main/java/whiteheadcrab/springframework/shoppingApp/ShoppingSteps.java
@@ -20,40 +20,21 @@ public class ShoppingSteps {
         System.out.println("In basket should be see selection of products");
     }
 
-    @When("User browse basket without buying product")
-    public void userViewBasketNoProducts()
+    @When("User browse basket {string} buying product")
+    public void userBrowseBasketBuyingProduct(String data)
     {
-        System.out.println("User browse basket without buying product");
+        System.out.println("User browse basket "+data+" buying product");
     }
 
-    @Then("User's basket should not be empty")
-    public void userViewBasketNotEmpty()
+    @Then("User's basket should {string} be empty")
+    public void userSBasketShouldBeEmpty(String data)
     {
-        System.out.println("User's basket should not be empty");
+        System.out.println("User's basket should "+data+" be empty");
     }
 
-    @And("User can buy an item")
-    public void userCanBuyItem()
+    @And("User {string} buy an item")
+    public void userBuyAnItem(String action)
     {
-        System.out.println("User can buy an item");
+        System.out.println("User "+action+" buy an item");
     }
-
-    @When("^User browse basket with buying product$")
-    public void userViewBasketWithProducts()
-    {
-        System.out.println("User browse basket with buying product");
-    }
-
-    @Then("^User's basket should be empty$")
-    public void userViewBasketEmpty()
-    {
-        System.out.println("User's basket should be empty");
-    }
-
-    @And("^User cannot buy an item$")
-    public void userCannotBuyItem()
-    {
-        System.out.println("User cannot buy an item");
-    }
-
 }
--- src/main/resources/features/shopping.feature
+++ src/main/resources/features/shopping.feature
@@ -3,13 +3,13 @@ Feature:  Check my basket application
   Scenario: No item Selected in basket
     Given User start the application
     Then In basket should be see selection of products
-    When User browse basket without buying product
-    Then User's basket should be empty
-    And User cannot buy an item
+    When User browse basket "without" buying product
+    Then User's basket should "" be empty
+    And User "cannot" buy an item
 
     Scenario: Item selected in basket
       Given User start the application
       Then In basket should be see selection of products
-      When User browse basket with buying product
-      Then User's basket should not be empty
-      And User can buy an item
+      When User browse basket "with" buying product
+      Then User's basket should "not" be empty
+      And User "can" buy an item
